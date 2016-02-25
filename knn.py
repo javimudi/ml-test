3e0accdcb37ae73d23b174f570eadc9d1c92ad94
Javier Muñoz Díaz <javi@MacBook-Pro.local>
2016-02-25 01:48:07 +0100

    Tests

--- knn.py
+++ knn.py
@@ -8,35 +8,36 @@ import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
 from datetime import datetime, timedelta
 
-def loadDataset(filename, split, trainingSet=[] , testSet=[]):
-	with open(filename, 'rb') as csvfile:
-	    lines = csv.reader(csvfile)
-	    dataset = list(lines)
-	    for x in range(1, len(dataset)-1):
-	        for y in range(4):
-	            dataset[x][y] = float(dataset[x][y])
-	        if random.random() < split:
-	            trainingSet.append(dataset[x])
-	        else:
-	            testSet.append(dataset[x])
+# def loadDataset(filename, split, trainingSet=[] , testSet=[]):
+# 	with open(filename, 'rb') as csvfile:
+# 	    lines = csv.reader(csvfile)
+# 	    dataset = list(lines)
+# 	    for x in range(1, len(dataset)-1):
+# 	        for y in range(4):
+# 	            dataset[x][y] = float(dataset[x][y])
+# 	        if random.random() < split:
+# 	            trainingSet.append(dataset[x])
+# 	        else:
+# 	            testSet.append(dataset[x])
 
-	return trainingSet, testSet
+	# return trainingSet, testSet
 
 def loadFullDataset(filename):
 	with open(filename, 'rb') as csvfile:
-	    lines = csv.reader(csvfile)
-	    return list(lines)
+		lines = csv.reader(csvfile)
+		return list(lines)
 
-def prepareDataset(dataset, split, trainingSet=[], testSet=[]):
-    for x in range(1, len(dataset)-1):
-        for y in range(4):
-	            dataset[x][y] = float(dataset[x][y])
-        if random.random() < split:
-            trainingSet.append(dataset[x])
-        else:
-            testSet.append(dataset[x])
-    # print trainingSet, testSet
-    return trainingSet, testSet
+def prepareDataset(dataset, split):
+	trainingSet = list()
+	testSet = list()
+	for line in dataset[1:]:
+		for elem in line[1:4]:
+			if random.random() < split:
+				trainingSet.append(float(elem))
+			else:
+				testSet.append(float(elem))
+	print trainingSet
+	return trainingSet, testSet
 
 def euclideanDistance(instance1, instance2, length):
 	distance = 0
@@ -51,11 +52,8 @@ def getNeighbors(trainingSet, testInstance, k):
 		dist = euclideanDistance(testInstance, trainingSet[x], length)
 		distances.append((trainingSet[x], dist))
 
-	
-	# print distances	
 	distances.sort(key=operator.itemgetter(1))
-
-
+	
 	for x in range(k):
 		yield distances[x][0]
 
@@ -88,30 +86,23 @@ def getAccuraciesNP(testSet, predictions):
 def main():
 
 	dataset = loadFullDataset('iris.csv')
-
 	for inter in range(1,1000):
 		split = float(inter)/100
 		# prepare data
-		lenTrainingSet = []
-		predictions = []
-		repetitions = 10	
+		repetitions = 5	
 		for rep in xrange(repetitions):
-			print split
+			lenTrainingSet = []
 			trainingSet, testSet = prepareDataset(dataset, split)
 			lenTrainingSet.append(len(trainingSet))
 			k = 3
+			predictions = []
 			for testElem in testSet:
 				neighbors = getNeighbors(trainingSet, testElem, k)
 				predictions.append(getResponse(neighbors))
 
-			# then = datetime.utcnow()
+			print len(testSet), len(predictions)
 			accuracy = getAccuracies(testSet, predictions)
-			# acc1 = datetime.utcnow()-then
 			print('Accuracy: ' + repr(accuracy) + '%'),
-			# then = datetime.utcnow()
-			# print getAccuraciesNP(testSet, predictions)
-			# acc2 = datetime.utcnow()-then
-			# print acc1, acc2
 			print "Thres: {0}, Latest Len Training set: {1}".format(split, lenTrainingSet)
 	
 main()
